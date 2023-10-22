07f77666e6d8c949ddb95bfc1d948b51b08ffa27
Tim Yang <76874480+Tim-Saijun@users.noreply.github.com>
2023-10-22 20:24:15 +0800

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -23,8 +23,8 @@ function App() {
           ))}
         </div>
         <div id='footer'>
-          <p>基于 <Link to='https://uptimerobot.com/' text='UptimeRobot' /> 接口制作，检测频率 5 分钟</p>
-          <p>&copy; 2020 <Link to='https://status.org.cn/' text='STATUS.ORG.CN' />, Version {Package.version}</p>
+          <p>检测频率 5 分钟，如果网站状态正常，而您无法访问，请检查您的网络代理</p>
+          <p>&copy; 2023 <Link to='https://www.zair.top/' text='以太工坊' />, Version {Package.version}</p>
         </div>
       </div>
     </>
