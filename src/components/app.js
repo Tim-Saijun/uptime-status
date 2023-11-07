d7a027fdbdcd54310e524906cbeb6ec9df3c9104
Tim Yang <76874480+Tim-Saijun@users.noreply.github.com>
2023-11-07 15:08:49 +0800

    添加Rss

--- src/components/app.js
+++ src/components/app.js
@@ -24,9 +24,7 @@ function App() {
         </div>
         <div id='footer'>
           <p>检测频率 5 分钟，如果网站状态正常，而您无法访问，请检查您的网络代理</p>
-          <p>&copy;</p>
-          <p><Link to='https://rss.uptimerobot.com/u2344984-b51264a6adbb1b5d110dbd5e09fa05b0' text=' ' /></p>
-          <p>2023 <Link to='https://www.zair.top/' text='以太工坊' />, Version {Package.version}</p>
+          <p>&copy;<Link to='https://rss.uptimerobot.com/u2344984-b51264a6adbb1b5d110dbd5e09fa05b0' text='2' />023 <Link to='https://www.zair.top/' text='以太工坊' />, Version {Package.version}</p>
         </div>
       </div>
     </>
